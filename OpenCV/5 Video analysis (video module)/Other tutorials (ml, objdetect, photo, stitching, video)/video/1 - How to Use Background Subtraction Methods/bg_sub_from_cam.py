b53c3b1e5c3ecc5547890d3f7a3dc59a4d2b93fc
Tyrone Edwards <tyrone.edwards1@btinternet.com>
2025-10-25 19:00:40 +0100

    Add files via upload

---
+++ OpenCV/5 Video analysis (video module)/Other tutorials (ml, objdetect, photo, stitching, video)/video/1 - How to Use Background Subtraction Methods/bg_sub_from_cam.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+import cv2 as cv
+import argparse
+
+parser = argparse.ArgumentParser(description='this program shows how to use background subtraction methods \
+                                 by OPENCV. you can process both vids and imgs')
+parser.add_argument('--input', type=str, help='path to vid or sequence of imgs', default='vid.mov')
+parser.add_argument('--algo', type=str, help='background subtraction method (KNN,MOG2)', default='MOG2')
+args = parser.parse_args()
+
+
+if args.algo == 'MOG2':
+    backSub = cv.createBackgroundSubtractorMOG2()
+else:
+    backSub = cv.createBackgroundSubtractorKNN()
+
+capture = cv.VideoCapture(0)
+if not capture.isOpened():
+    print('unable to open cam')
+    exit(0)
+
+while True:
+    ret, frame = capture.read()
+    if frame is None:
+        break
+
+    fgMask = backSub.apply(frame)
+
+    cv.rectangle(frame, (10,2), (100,20), (255,255,255), -1)
+
+    # this is the vid frame text in the corner 
+    cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15,15,), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0)) 
+
+    cv.imshow('Frame', frame)
+    cv.imshow('FG Mask', fgMask)
+
+    if cv.waitKey(30) & 0xff == 27:
+        break
+        cv.destroyAllWindows()
+
+# When everything done, release the capture
+capture.release()
+cv.destroyAllWindows()
